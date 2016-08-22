828785a3adac13ba7dcab0c5be354e7938490752
zangkun <04041143@163.com>
2016-08-22 21:58:26 +0800

    新手村

--- worlds/plugins/newbie.lua
+++ worlds/plugins/newbie.lua
@@ -10,34 +10,3 @@ function player.setGender(gender)
         player.gender = 2
     end
 end
-
-function player.eatYeguo()
-    wait.make(function()
-        while true do
-            Execute("get ye guo")
-            local line = wait.regexp("^[> ]*你身上已经有野果了，拿多了也是浪费。")
-            if line ~= nil then
-                break
-            else
-                line = wait.regexp("^[> ]*你捡起一枚野果.*", 5)
-                if line ~= nil then
-                    break
-                end
-            end
-        end
-
-        Execute("eat ye guo")
-    end)
-end
-
-function player.getHulu()
-    wait.make(function()
-        while true do
-            Execute("get hulu")
-            local line = wait.regexp(".*你捡起一个葫芦.*", 5)
-            if line ~= nil then
-                break
-            end
-        end
-    end)
-end
